61b5a48e1157f6ec73be195be9d62cd1c0edaf61
OisinWrites <oisin.banville@gmail.com>
2024-04-15 11:29:40 +0100

    Corrected util, changed client dir, custom favico

--- client/src/utils.js
+++ client/src/utils.js
@@ -16,7 +16,8 @@ export function setCookie(name, value, days) {
 
 export function generatePlayerId() {
     return 'xxxx-xxxx-xxxx'.replace(/[x]/g, function(c) {
-        const r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
+        const r = Math.random() * 16 | 0;
+        const v = c === 'x' ? r : (r & 0x3) | 0x8;
         return v.toString(16);
     });
 }
